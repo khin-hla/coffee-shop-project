f66c0fe5b9c9a2740103c1cf49d84a093d43b58c
khin-hla <119106104+khin-hla@users.noreply.github.com>
2023-03-11 07:31:14 +0630

    configure or deployment

---
+++ src/add-member.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form')
+const nameInput = document.querySelector('#name')
+const jobInput = document.querySelector('#job')
+const submitBtn = document.querySelector('#submit-btn');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+   submitBtn.textContent = 'Loading...'
+    
+    const data = {
+        name: nameInput.value,
+        job: jobInput.value,
+    };
+
+    try{
+        const res = await fetch('https://reqres.in/api/users',{
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+    
+            body: JSON.stringify(data)
+        })
+    
+        if(res.ok){
+            alert('Successful')
+            submitBtn.textContent = 'Save';
+        }else{
+            alert("unsuccessful")
+        }
+    }catch(error){
+       throw new Error(error);
+    }
+})
